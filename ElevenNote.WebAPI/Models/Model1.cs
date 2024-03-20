f1a803850b62e3145c6860a5a536340c41b08fa8
Zachary Amstutz <zachamstutz1@gmail.com>
2024-03-20 17:44:26 -0400

    Added in refernces for services, models, and data

---
+++ ElevenNote.WebAPI/Models/Model1.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElevenNote.WebAPI.Models
+{
+    public class Model1
+    {
+        
+    }
+}
