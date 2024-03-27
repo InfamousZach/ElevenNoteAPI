f46efda55f6432cd5cd237f551d82d3fc72fd4fb
Zachary Amstutz <zachamstutz1@gmail.com>
2024-03-27 18:56:29 -0400

    added in Register Controller, Register Refractor, and Passowrd Hasher.

--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ElevenNote.Services.User;
+using ElevenNote.Models.User;
 
 namespace MyApp.Namespace
 {
@@ -8,6 +9,22 @@ namespace MyApp.Namespace
     [ApiController]
     public class UserController : ControllerBase
     {
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            var registerResult = await _userService.RegisterUserAsync(model);
+            if (registerResult)
+            {
+                return Ok("User was registered.");
+            }
+
+            return BadRequest("User could not be registered.");
+        }
         private readonly IUserService _userService;
         public UserController(IUserService userService)
         {
