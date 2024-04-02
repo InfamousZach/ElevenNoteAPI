e86420f1116f824e37523ce23eda18c616df5dbb
Zachary Amstutz <zachamstutz1@gmail.com>
2024-04-02 19:09:53 -0400

    added in the token endpoint

--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using ElevenNote.Services.User;
 using ElevenNote.Models.User;
 using Microsoft.AspNetCore.Authorization;
+using ElevenNote.Services.Token;
+using ElevenNote.Models.Token;
 
 namespace MyApp.Namespace
 {
@@ -40,10 +42,26 @@ namespace MyApp.Namespace
 
             return Ok(userDetail);
         }
+
+        [HttpPost("~/api/Token")]
+        public async Task<IActionResult> GetToken([FromBody] TokenRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var tokenResponse = await _tokenService.GetTokenAsync(request);
+            if (tokenResponse is null)
+                return BadRequest("Invalid username or password.");
+
+            return Ok(tokenResponse);
+        }
+
         private readonly IUserService _userService;
-        public UserController(IUserService userService)
+        private readonly ITokenService _tokenService;
+        public UserController(IUserService userService, ITokenService tokenService)
         {
             _userService = userService;
+            _tokenService = tokenService;
         }
     }
 }
